597ab2801d5746fea362593a32048f402d7a50c4
elisabichiappini <elisabichiappini@gmail.com>
2023-11-23 00:46:55 +0100

    feat: snack 3

---
+++ snack3/js/myscript.js
@@ -0,0 +1,19 @@
+/***************************************************************
+JAVASCRIPT
+****************************************************************/
+
+'use strict';
+// Bonus Snack 3
+// - Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// - La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Usare i metodi degli array foreach o filter.
+
+function checkFn (array, a, b) {
+    console.log(array);
+    console.log(b - a);
+}
+
+const newArray = [];
+
+checkFn(newArray, 2, 4);
+
+console.log(checkFn);
