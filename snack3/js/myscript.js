384c3b9b0dd819d95c908b3f10073b0b00a459f2
elisabichiappini <elisabichiappini@gmail.com>
2023-11-23 11:35:36 +0100

    feat: snack 3 scaffolding solution

--- snack3/js/myscript.js
+++ snack3/js/myscript.js
@@ -7,13 +7,28 @@ JAVASCRIPT
 // - Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // - La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Usare i metodi degli array foreach o filter.
 
-function checkFn (array, a, b) {
-    console.log(array);
-    console.log(b - a);
+function nuovoArray (array, aMin, bMax) {
+    switch (true) {
+        case !Array.isArray(array):
+        case aMin > bMax:
+        case aMin < 0 || bMax > array.length - 1:
+         return -1;
+
+        default: 
+            return array.filter((element, index) => index >= aMin && index <= bMax);
+    }
 }
 
-const newArray = [];
+const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-checkFn(newArray, 2, 4);
+const result1 = nuovoArray(numbers, 2, 4);
+const result2 = nuovoArray(numbers, 4, 7);
+const result3 = nuovoArray(numbers, -5, 4);
+const result4 = nuovoArray(numbers,1, 5);
+const result5 = nuovoArray('pippo', 5, 2);
 
-console.log(checkFn);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
